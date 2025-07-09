e6a7b95a41777ddee498615dc5f5007c19d09656
roger06 <r.holden@chi.ac.uk>
2025-07-09 15:52:01 +0100

    app

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>it's a pain in the bum on git hub pages!</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
